c5812fadb94ac9035b7bcf5c0ca1e6a1b7fbcb30
Shyam Sharma <121392308+shyamsharma15@users.noreply.github.com>
2022-12-25 22:15:13 +0530

    Switch Case In C++

---
+++ switch.cpp
@@ -0,0 +1,30 @@
+// Program for making a simple calculator by using the switch case :) ....
+/*
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    char op;
+    cout<<" enter the first number "<<endl;
+    cin>>a;
+    cout<<" enter the second number "<<endl;
+    cin>>b;
+    cout<<" enter the operation which you perform "<<endl;
+    cin>>op;
+    switch(op)
+    {
+        case '+': cout<<" the sum is "<<(a+b)<<endl;
+                      break;
+        case '-' : cout<<" the difference is "<<(a-b)<<endl;
+                      break;
+        case '*' : cout<<" the multiplication is "<<(a*b)<<endl;
+                      break;
+        case '%' : cout<<" the modullo is "<<(a%b)<<endl;
+                      break;
+
+    }
+
+}
+*/
+
